71addca4721fce13594c8311b8d57a53d9e0b4e0
Sebastián Kissling <sebastian.kissling@gmail.com>
2019-10-20 16:11:43 -0300

    Agregar clase MMServer que inicia todos los recursos

--- server/main.cpp
+++ server/main.cpp
@@ -1,49 +1,17 @@
-#include "include/acceptor_th.h"
-#include "include/game_loop_th.h"
-#include "include/micromachines.h"
-#include "../common/include/socket_error.h"
+#include "./include/mm_server.h"
 #include <iostream>
-#include <string>
-#include <vector>
 
 #define SUCCESS 0
 
-void startThreads(std::vector<Thread*> &threads) {
-    for (std::vector<Thread*>::iterator it = threads.begin();
-        it != threads.end(); it++) {
-        (*it)->start();
-    }
-}
-
-void finishThreads(std::vector<Thread*> &threads) {
-    for (std::vector<Thread*>::iterator it = threads.begin();
-        it != threads.end(); it++) {
-        (*it)->stop();
-        (*it)->join();
-    }
-}
-
-void run(const char* service) {
-    try {
-        Micromachines micromachines;
-        std::vector<Thread*> threads;
-        GameLoopTh game_loop_th(micromachines);
-        AcceptorTh acceptor_th(service, micromachines);
-        threads.push_back(&game_loop_th);
-        threads.push_back(&acceptor_th);
-        startThreads(threads);
-        while (getc(stdin) != 'q') {}
-        finishThreads(threads);
-    } catch(const SocketError &e) {
-        std::cout << e.what() << "\n";
-    }
-}
 
 int main(int argc, char const *argv[]) {
     if (argc != 2) {
         std::cout << "El comando es: ./micromachines-sv <service>\n";
         return SUCCESS;
     }
-    run(argv[1]);
+
+    MMServer server;
+    server.start(argv[1]);
+    
     return SUCCESS;
 }
---
+++ server/src/mm_server.cpp
@@ -0,0 +1,39 @@
+#include "../include/mm_server.h"
+#include "../include/acceptor_th.h"
+#include "../include/game_loop_th.h"
+#include "../include/micromachines.h"
+#include "../../common/include/thread.h"
+#include "../../common/include/socket_error.h"
+#include <iostream>
+#include <string>
+#include <vector>
+
+void MMServer::startThreads() {
+    for (std::vector<Thread*>::iterator it = threads.begin();
+        it != threads.end(); it++) {
+        (*it)->start();
+    }
+}
+
+void MMServer::finishThreads() {
+    for (std::vector<Thread*>::iterator it = threads.begin();
+        it != threads.end(); it++) {
+        (*it)->stop();
+        (*it)->join();
+    }
+}
+
+void MMServer::start(const char *service) {
+    try {
+        Micromachines micromachines;
+        GameLoopTh game_loop_th(micromachines);
+        AcceptorTh acceptor_th(service, micromachines);
+        threads.push_back(&game_loop_th);
+        threads.push_back(&acceptor_th);
+        startThreads();
+        while (getc(stdin) != 'q') {}
+        finishThreads();
+    } catch(const SocketError &e) {
+        std::cout << e.what() << "\n";
+    }
+}
