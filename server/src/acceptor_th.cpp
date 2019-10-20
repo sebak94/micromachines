36ae35eb38dbe0d940d7f1504ddf82f114606c31
Sebastián Kissling <sebastian.kissling@gmail.com>
2019-10-20 20:30:54 -0300

    Refactorizar metodo run de AcceptorTh

--- server/src/acceptor_th.cpp
+++ server/src/acceptor_th.cpp
@@ -15,35 +15,20 @@ AcceptorTh::AcceptorTh(const char *service, Micromachines micromachines):
     }
 }
 
-void AcceptorTh::run() {
-    std::vector<ClientTh*> clients;
-    std::vector<Socket*> sockets;
-
-    while (keep_accepting) {
-        try {
-            Socket *peer = new Socket();
-            sockets.push_back(peer);
-            skt.Accept(peer);
-            ClientTh *client_th = new ClientTh(peer);
-            clients.push_back(client_th);
-            client_th->start();
-            
-            std::vector<ClientTh*>::iterator it = clients.begin();
-            while (it != clients.end()) {
-                if ((*it)->isDead()) {
-                    (*it)->join();
-                    delete (*it);
-                    clients.erase(it);
-                } else {
-                    it++;
-                }
-            }
-        } catch(const SocketError &e) {
-            std::cout << e.what() << "\n";
-            keep_accepting = false;
+void AcceptorTh::deleteDeadClients() {
+    std::vector<ClientTh*>::iterator it = clients.begin();
+    while (it != clients.end()) {
+        if ((*it)->isDead()) {
+            (*it)->join();
+            delete (*it);
+            clients.erase(it);
+        } else {
+            it++;
         }
     }
+}
 
+void AcceptorTh::deleteResources() {
     for (size_t i = 0; i < clients.size(); i++) {
         clients[i]->stop();
         clients[i]->join();
@@ -53,6 +38,25 @@ void AcceptorTh::run() {
     for (size_t i = 0; i < sockets.size(); i++) {
         delete sockets[i];
     }
+}
+
+void AcceptorTh::run() {
+    while (keep_accepting) {
+        try {
+            Socket *peer = new Socket();
+            sockets.push_back(peer);
+            skt.Accept(peer);
+            ClientTh *client_th = new ClientTh(peer);
+            clients.push_back(client_th);
+            client_th->start();
+            deleteDeadClients();
+        } catch(const SocketError &e) {
+            std::cout << e.what() << "\n";
+            keep_accepting = false;
+        }
+    }
+
+    deleteResources();    
 }
 
 void AcceptorTh::stop() {
