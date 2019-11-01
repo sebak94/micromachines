1b634232a950f7b7e8c004b4659d28e1c56e7aea
MLahore <matiaslahore@gmail.com>
2019-11-01 11:27:57 -0300

    interpreter 0.1

---
+++ server/src/model/interpreter.cpp
@@ -0,0 +1,5 @@
+//
+// Created by mati on 11/1/19.
+//
+
+#include "interpreter.h"
---
+++ server/src/model/interpreter.h
@@ -0,0 +1,14 @@
+//
+// Created by mati on 11/1/19.
+//
+
+#ifndef MICROMACHINES_INTERPRETER_H
+#define MICROMACHINES_INTERPRETER_H
+
+
+class interpreter {
+
+};
+
+
+#endif //MICROMACHINES_INTERPRETER_H
