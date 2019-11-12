178478a32faeb06faf4b344f50265b67cf26d735
eunilewczuk <euni.lewczuk@hotmail.com>
2019-11-12 10:12:32 -0300

    Visualizacion de la salud del auto

--- client/src/Camera.cpp
+++ client/src/Camera.cpp
@@ -1,8 +1,15 @@
+#include <SDL2/SDL_ttf.h>
 #include "../include/Camera.h"
+#include "../../common/include/TextTexture.h"
+
+#define HEART "heart"
+#define FONTNAME "../common/fonts/OpenSans-Bold.ttf"
 
 Camera::Camera(SdlWindow &window, std::map<std::string, SdlSurface*> &pictures,
         std::map<trackPartType, SdlSurface*> &trackPictures) :
-        window(window), pictures(pictures), trackPictures(trackPictures) {}
+        window(window), pictures(pictures), trackPictures(trackPictures) {
+    TTF_Init();
+}
 
 Camera::~Camera() {}
 
@@ -42,13 +49,22 @@ void Camera::showCars(int xMyCar, int yMyCar, std::map<std::string, Car*> &cars)
     double heightCar = blockHeight / 3;
     double xBegin = - xMyCar * (blockWidth / 100) + (window.getWidth() / 2);
     double yBegin = - yMyCar * (blockHeight / 100) - (window.getHeight() / 2);
+    SDL_Color color = {0, 0, 0, 0};
+    TextTexture text;
 
     for (std::map<std::string, Car*>::iterator it = cars.begin(); it != cars.end(); ++it) {
         Car* car = it->second;
         double x = car->getX() * (blockWidth / 100) - (widthCar / 2);
         double y = car->getY() * (blockHeight / 100) + (heightCar / 2);
-        SDL_Rect sdlDestCar = {(int)(x + xBegin), (int)(- y - yBegin), (int)widthCar, (int)heightCar};
+        int realX = x + xBegin;
+        int realY = - y - yBegin;
+        SDL_Rect sdlDestCar = {realX, realY, (int)widthCar, (int)heightCar};
         pictures[car->getMyColor()]->renderRotate(sdlDestCar, car->getDegrees(), SDL_FLIP_NONE);
+
+        SDL_Rect sdlDestHeart = {realX, (int)(realY+heightCar), (int)blockWidth / 20, (int)blockHeight / 20};
+        pictures[HEART]->render(sdlDestHeart);
+        text.textToTexture(window.getRenderer(), std::to_string(car->getHealth()) + "%", color, FONTNAME, blockWidth / 26);
+        text.render(window.getRenderer(), (int)(realX+sdlDestHeart.w), (int)(realY+heightCar));
     }
 }
 
