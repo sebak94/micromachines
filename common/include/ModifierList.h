2833839177d852ec5459cdf2fc8d5d2d1a125c93
fperezboco <fedeboco@gmail.com>
2019-11-24 22:35:50 -0300

    Throwing modifiers from grandstands.

--- common/include/ModifierList.h
+++ common/include/ModifierList.h
@@ -14,7 +14,6 @@ private:
     std::map<int, Modifier> modifiers;
     std::map<int, int> distance;
     int N = 1;
-
     std::string parse(const std::string &str, size_t &pos, const char delim);
 
 public:
@@ -24,11 +23,8 @@ public:
     std::string serialize();
     std::vector<Modifier> getModifiers();
     bool isOnBoost(int posX, int posY);
-
     void append(int x, int y, std::string type, direction dir);
-
     void updateDistance();
-
     void eraseModifier(int i);
 };
 
