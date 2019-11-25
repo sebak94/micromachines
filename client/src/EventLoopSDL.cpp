38f69facf6db2b178648387126a582b0d053ad8e
eunilewczuk <euni.lewczuk@hotmail.com>
2019-11-25 01:04:04 -0300

    Fix crear partida por 2da vez

--- client/src/EventLoopSDL.cpp
+++ client/src/EventLoopSDL.cpp
@@ -102,9 +102,9 @@ void EventLoopSDL::run() {
                 break;
             case startCountdown:
                 quitAndResize(event);
-                selectionSent = false;
                 break;
             case playing:
+                selectionSent = false;
                 quitAndResize(event);
                 if (!luaPlaying) {
                     switch (event.type) {
